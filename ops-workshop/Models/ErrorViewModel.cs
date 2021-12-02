73fe85d3d9778ff3ee66b38993363cdd9e9bcf64
Dennis Bouma <dennis.bouma@visma.com>
2021-12-02 10:42:44 +0100

    first commit

---
+++ ops-workshop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ops_workshop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
